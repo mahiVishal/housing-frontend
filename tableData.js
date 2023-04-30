de6b6e6d67b8f83e4805629ca4e4b39c3308fbf9
Vishal <mahivishal183@gmail.com>
2023-04-30 12:33:37 +0530

    init

---
+++ tableData.js
@@ -0,0 +1,19 @@
+const table = document.getElementById("table");
+
+fetch("http://localhost:3000/pg")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    displayData(data);
+  });
+
+function displayData(data) {
+  data.forEach((item) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+            <td>${item.name}</td>
+            <td>${item.contactNum}</td>
+        `;
+    table.appendChild(row);
+  });
+}
